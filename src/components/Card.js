27d32e4e2157ae8b91618cb87ec4b3b633fdc29e
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-08-09 13:20:32 -0400

    Add alt attributes to img elements

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import Star from "../img/star.png"
 export default function Card(props){
     return(
         <div className="card">
-            <img src={require(`../img/${props.img}`)} className="experience-cover-img"></img>
+            <img src={require(`../img/${props.img}`)} alt="" className="experience-cover-img"></img>
             <br></br>
             <div className="card-body">
-                <span><img src={Star}></img> {props.rating} <span className="text-light align-middle">({props.reviewCount}) - {props.location}</span></span>
+                <span><img src={Star} alt="Star Rating"></img> {props.rating} <span className="text-light align-middle">({props.reviewCount}) - {props.location}</span></span>
                 <p>{props.title}</p>
                 <p><strong>From ${props.price}</strong> / person</p>
             </div>
